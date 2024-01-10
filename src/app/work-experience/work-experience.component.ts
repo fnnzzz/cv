ce45a12e63217be923d9a946627a3515e40a1f37
Vladyslav Feninets <Vladyslav.Feninets@space-whale.io>
2024-01-10 18:48:37 +0200

    add total years counter, styles fixes

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -14,4 +14,11 @@ export class WorkExperienceComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  get getTotalYearsInIT(): string {
+    const firstYearInIT = 2015
+    const currentYear = new Date().getFullYear()
+    const diff = currentYear - firstYearInIT
+    return diff.toString()
+  }
+
 }
